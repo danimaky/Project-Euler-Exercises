a1b9fe364fb3b280b196df66fa3834826e862012
Daniel Nuñez <danielsistem.ing96@gmail.com>
2018-06-27 17:11:59 -0500

    Problem 12

--- Problem 12.py
+++ Problem 12.py
@@ -1,2 +1,14 @@
-i = 0
+def countmultiples(number):
+    count = len([i for i in range(1, number+1) if number % i == 0])
+    print(count)
+    return count
+
+i = 1000
 while True:
+    triangulenum = 0
+    for n in reversed(range(i+1)):
+        triangulenum += n
+    if countmultiples(triangulenum) > 500:
+        break
+    i += 1
+print(triangulenum)
